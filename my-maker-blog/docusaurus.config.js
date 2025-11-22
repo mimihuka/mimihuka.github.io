b2c9018b2b809cca916c19da412a403fd161514f
leosut <djjdnebdjxknf@gmail.com>
2025-11-22 11:11:49 +0800

    change project and organization name

--- my-maker-blog/docusaurus.config.js
+++ my-maker-blog/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://mimihuka.github.io/',
+  url: 'https://mimihuka.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/my-maker-blog/',
